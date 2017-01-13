7f2667dad54c2fc8d0542bd0a36ba1cc35783065
Andrei Bejenaru <andrei.bejenaru@ext.ec.europa.eu>
2017-01-13 15:23:34 +0100

    updates prior to automatic run for 012017_1523

---
+++ automatic/etcd/tools/chocolateyInstall.ps1
@@ -0,0 +1,37 @@
+﻿# stop on all errors
+$ErrorActionPreference = 'Stop';
+
+$packageName    = '{{{{PackageName}}}}'
+$packageVersion = '{{PackageVersion}}'
+$url64          = '{{DownloadUrlx64}}'
+$checksum64     = '{{Checksumx64}}'
+$checksumType64 = '{{ChecksumTypex64}}'
+
+$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installDir     = "C:\ProgramData\etcd"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" -Url64 "$url64" -UnzipLocation "$installDir" -ChecksumType64 "$checksumType64" -Checksum64 "$checksum64"
+
+# check OS bitness
+if (!([Environment]::Is64BitOperatingSystem)) {
+	Write-Error "etcd requires 64bit operating system"
+	exit
+}
+
+Copy-Item "C:\ProgramData\etcd\etcd-v$packageVersion-windows-amd64\*" C:\ProgramData\etcd -Recurse -Force
+Remove-Item "C:\ProgramData\etcd\etcd-v$packageVersion-windows-amd64" -Recurse -Force
+
+# older versions of etcd didn't put EXE on the binary
+if (Test-Path C:\ProgramData\etcd\etcd) {
+    copy-item C:\ProgramData\etcd\etcd C:\ProgramData\etcd\etcd.exe -force
+    remove-item C:\ProgramData\etcd\etcd
+}
+if (Test-Path C:\ProgramData\etcd\etcdctl) {
+    copy-item C:\ProgramData\etcd\etcdctl C:\ProgramData\etcd\etcdctl.exe -force
+    remove-item C:\ProgramData\etcd\etcdctl
+}
+
+Copy-Item C:\ProgramData\etcd\etcdctl.exe $toolsDir -Force
+&nssm install etcd C:\ProgramData\etcd\etcd.exe "$($env:chocolateyPackageParameters)"
+&nssm set etcd Start SERVICE_AUTO_START
---
+++ automatic/etcd/tools/chocolateyUninstall.ps1
@@ -0,0 +1,7 @@
+﻿# stop on all errors
+$ErrorActionPreference = 'Stop';
+
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Remove-Item C:\ProgramData\etcd -Recurse -Force
+Remove-Item "$toolsDir\etcdctl.exe"
