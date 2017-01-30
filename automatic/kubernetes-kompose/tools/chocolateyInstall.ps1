24ebd2baabbb99074b593a70369eb7b769a21958
Andrei Bejenaru <andrei.bejenaru@ext.ec.europa.eu>
2017-01-30 12:30:00 +0100

    updates prior to automatic run for 012017_1230

--- automatic/kubernetes-kompose/tools/chocolateyInstall.ps1
+++ automatic/kubernetes-kompose/tools/chocolateyInstall.ps1
@@ -1,22 +1,10 @@
 ﻿$packageName    = '{{PackageName}}'
-$packageVersion = '{{PackageVersion}}'
-$url            = '{{DownloadUrl}}'
-$url64          = '{{DownloadUrlx64}}'
-$checksum       = '{{Checksum}}'
-$checksum64     = '{{Checksumx64}}'
-$checksumType   = 'sha256'
+$url64          = '{{DownloadUrl}}'
+$checksum64     = '{{Checksum}}'
 $checksumType64 = 'sha256'
 
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$tempDir    = "$toolsDir\temp"
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileFullPath   = "$toolsDir\kompose.exe"
 
 
-# Download and unzip into a temp folder
-Install-ChocolateyZipPackage -PackageName "$packageName" -Url "$url" -Url64 "$url64" -UnzipLocation "$tempDir" -ChecksumType "$checksumType" -Checksum "$checksum" -ChecksumType64 "$checksumType64" -Checksum64 "$checksum64"
-
-# Untar into tools folder
-Get-ChocolateyUnzip -PackageName "$packageName" -FileFullPath "$tempDir" -Destination "$toolsDir"
-
-# Remove temp folder
-Remove-Item "$tempDir\*" -recurse
-Remove-Item "$tempDir"
+Get-ChocolateyWebFile -PackageName "$packageName" -FileFullPath "$fileFullPath" -Url64 "$url64" -ChecksumType64 "$checksumType64" -Checksum64 "$checksum64"
--- automatic/minikube/tools/chocolateyInstall.ps1
+++ automatic/minikube/tools/chocolateyInstall.ps1
@@ -1,11 +1,10 @@
 ﻿$packageName    = '{{PackageName}}'
-$packageVersion = '{{PackageVersion}}'
 $url64          = '{{DownloadUrl}}'
 $checksum64     = '{{Checksum}}'
 $checksumType64 = 'sha256'
 
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$fileFullPath   = "$toolsDir\$packageName.exe"
+$fileFullPath   = "$toolsDir\minikube.exe"
 
 
 Get-ChocolateyWebFile -PackageName "$packageName" -FileFullPath "$fileFullPath" -Url64 "$url64" -ChecksumType64 "$checksumType64" -Checksum64 "$checksum64"
