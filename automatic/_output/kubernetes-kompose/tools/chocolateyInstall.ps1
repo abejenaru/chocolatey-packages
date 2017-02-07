f4e1f95e3f24a40147291b0e4504b1029880854c
Andrei Bejenaru <andrei.bejenaru@ext.ec.europa.eu>
2017-02-07 08:30:00 +0100

    updates prior to automatic run for 022017_ 830

--- automatic/_output/kubernetes-kompose/tools/chocolateyInstall.ps1
+++ automatic/_output/kubernetes-kompose/tools/chocolateyInstall.ps1
@@ -1,13 +1,13 @@
 $packageName    = 'kubernetes-kompose'
 $url64          = 'https://github.com/kubernetes-incubator/kompose/releases/download/v0.2.0/kompose-windows-amd64.exe'
-$checksum64     = '53dc9fcb4b2328bc9c57ec8a465d2dc038a8927845b0a32d4500e5e7d5fb897a'
+$checksum64     = '43cf92a28a3c94a43a474fd2eaa3285dc4a8448453eff3b7357191db2995e351'
 $checksumType64 = 'sha256'
 
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $fileFullPath   = "$toolsDir\kompose.exe"
 
 
-Get-ChocolateyWebFile -PackageName "$packageName" -FileFullPath "$fileFullPath" -Url64 "$url64" -ChecksumType64 "$checksumType64" -Checksum64 "$checksum64"
+Get-ChocolateyWebFile -PackageName "$packageName" -FileFullPath "$fileFullPath" -Url64 "$url64" -Checksum64 "$checksum64" -ChecksumType64 "$checksumType64"
 
 # Remove tools sub-folders (previous package installations)
 $subdirs = Get-ChildItem "$toolsDir" -Directory
