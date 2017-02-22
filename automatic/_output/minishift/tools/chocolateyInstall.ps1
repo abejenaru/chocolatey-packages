80636d7c62198fe1fe80bdc1e29d39c9e4f42151
Andrei Bejenaru <andrei.bejenaru@ext.ec.europa.eu>
2017-02-22 08:30:00 +0100

    updates prior to automatic run for 022017_ 830

--- automatic/_output/minishift/tools/chocolateyInstall.ps1
+++ automatic/_output/minishift/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $packageName    = 'minishift'
-$url64          = 'https://github.com/minishift/minishift/releases/download/v1.0.0-beta.3/minishift-1.0.0-beta.3-windows-amd64.zip'
-$checksum64     = 'b4b1d874cf709c1c91e2a3c2bd3de855189ca32bd19f557c10099c149ea32384'
+$url64          = 'https://github.com/minishift/minishift/releases/download/v1.0.0-beta.4/minishift-1.0.0-beta.4-windows-amd64.zip'
+$checksum64     = '6ec37a8bb4e8325213fb175525a14b60c153b3f2d88c0f06aebeae15524aac83'
 $checksumType64 = 'sha256'
 
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
