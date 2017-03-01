7f81f6306e063d1620884475cf2955e91edd33d5
Andrei Bejenaru <andrei.bejenaru@ext.ec.europa.eu>
2017-03-01 18:30:02 +0100

    updates prior to automatic run for 032017_1830

--- automatic/_output/gmkvextractgui/tools/chocolateyInstall.ps1
+++
@@ -1,9 +0,0 @@
-$packageName    = 'gmkvextractgui'
-$url            = 'https://sourceforge.net/projects/gmkvextractgui/files/v1.9.0/gMKVExtractGUI.v1.9.0.7z/download'
-$checksum       = 'a4d9897ab2a53bf3ac69146e462875c390af634a8aafb98b8a2f3516d7098883'
-$checksumType   = 'sha256'
-
-$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-
-
-Install-ChocolateyZipPackage -PackageName "$packageName" -Url "$url" -UnzipLocation "$toolsDir" -ChecksumType "$checksumType" -Checksum "$checksum"
--- automatic/_output/microsoft-r-open/tools/chocolateyUninstall.ps1
+++
@@ -1,7 +0,0 @@
-
-# Remove desktop shortcut
-$desktop = $([System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::DesktopDirectory))
-$link = Join-Path $desktop "Microsoft R Open.lnk"
-If (Test-Path $link) {
-    Remove-Item "$link"
-}
