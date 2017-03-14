bc84e0a6d87e88153789c4af53f20a066af79e55
Andrei Bejenaru <andrei.bejenaru@ext.ec.europa.eu>
2017-03-14 12:30:00 +0100

    updates prior to automatic run for 032017_1230

---
+++ automatic/fujifilm-rfc/tools/chocolateyInstall.ps1
@@ -0,0 +1,23 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName= 'fujifilm-rfc'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = '{{DownloadUrl}}'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+
+  softwareName  = 'fujifilm-rfc*'
+
+  checksum      = '{{Checksum}}'
+  checksumType  = 'sha256'
+
+  # InstallShield
+  silentArgs   = '/s'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
