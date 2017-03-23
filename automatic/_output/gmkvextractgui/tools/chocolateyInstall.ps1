7c2917a920834f2cd97f415342446333bd585b43
Andrei Bejenaru <andrei.bejenaru@ext.ec.europa.eu>
2017-03-23 12:30:00 +0100

    updates prior to automatic run for 032017_1230

--- automatic/_output/gmkvextractgui/tools/chocolateyInstall.ps1
+++
@@ -1,9 +0,0 @@
-$packageName    = 'gmkvextractgui'
-$url            = 'https://sourceforge.net/projects/gmkvextractgui/files/v1.9.1/gMKVExtractGUI.v1.9.1.7z/download'
-$checksum       = '27121eea5f6858cbd006db507f766cadc70d4f75c1b1ac31e95a0035d63a876d'
-$checksumType   = 'sha256'
-
-$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-
-
-Install-ChocolateyZipPackage -PackageName "$packageName" -Url "$url" -UnzipLocation "$toolsDir" -ChecksumType "$checksumType" -Checksum "$checksum"
--- automatic/_output/minishift/tools/chocolateyInstall.ps1
+++
@@ -1,9 +0,0 @@
-$packageName    = 'minishift'
-$url64          = 'https://github.com/minishift/minishift/releases/download/v1.0.0-beta.5/minishift-1.0.0-beta.5-windows-amd64.zip'
-$checksum64     = '759621e2895a42b785fcd0f9cf7454809cfbd7aec8e2119e1d35b9a378163158'
-$checksumType64 = 'sha256'
-
-$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-
-
-Install-ChocolateyZipPackage -PackageName "$packageName" -UnzipLocation "$toolsDir" -Url64 "$url64" -Checksum64 "$checksum64" -ChecksumType64 "$checksumType64"
