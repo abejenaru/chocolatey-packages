b61e42de84ee1125a9f6c7ef959cd3855ebb8d30
Andrei Bejenaru <andrei.bejenaru@ext.ec.europa.eu>
2017-04-21 12:30:00 +0200

    updates prior to automatic run for 042017_1230

--- automatic/_output/kotlin/tools/chocolateyBeforeModify.ps1
+++
@@ -1,9 +0,0 @@
-$packageName    = $env:chocolateyPackageName
-$packageVersion = $env:chocolateyPackageVersion
-
-$toolsDir       = Get-ToolsLocation
-$kotlin_home    = Join-Path $toolsDir "$packageName-$packageVersion"
-
-Remove-Item $kotlin_home -Recurse -Force
-
-Install-ChocolateyEnvironmentVariable -VariableName 'KOTLIN_HOME' -VariableValue $null -VariableType 'Machine'
