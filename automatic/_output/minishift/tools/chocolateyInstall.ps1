e8c38f709299904615ab31c13845162f33987de3
Andrei Bejenaru <andrei.bejenaru@ext.ec.europa.eu>
2018-02-23 10:33:36 +0100

    Clean private Choco packages repo; move kubernetes-cli and kubernetes-kompose to Core team repo

--- automatic/_output/minishift/tools/chocolateyInstall.ps1
+++
@@ -1,9 +0,0 @@
-$packageName    = 'minishift'
-$url64          = 'https://github.com/minishift/minishift/releases/download/v1.0.0-rc.2/minishift-1.0.0-rc.2-windows-amd64.zip'
-$checksum64     = 'f68c7dd70865bd260d0b005a25f564522013ceca7685f7e7568f4ab8e0cf5ff9'
-$checksumType64 = 'sha256'
-
-$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-
-
-Install-ChocolateyZipPackage -PackageName "$packageName" -UnzipLocation "$toolsDir" -Url64 "$url64" -Checksum64 "$checksum64" -ChecksumType64 "$checksumType64"
--- automatic/_output/openshift-cli/tools/chocolateyInstall.ps1
+++
@@ -1,8 +0,0 @@
-$packageName    = 'openshift-cli'
-$url            = 'https://github.com/openshift/origin/releases/download/v1.5.0/openshift-origin-client-tools-v1.5.0-031cbe4-windows.zip'
-$checksum       = 'e4650d9a53678141c17147a98670fc842fc78049762877def4cb66e385aadee7'
-$checksumType   = 'sha256'
-
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-
-Install-ChocolateyZipPackage -PackageName "$packageName" -UnzipLocation "$toolsDir" -Url "$url" -Checksum "$checksum" -ChecksumType "$checksumType"
